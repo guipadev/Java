8cd61284deaf9b1f87910f45776148cb499abbe5
yadevom <yaormu@gmail.com>
2023-07-02 20:53:53 -0500

    primer datos de java

---
+++ Java_17/1_descripcion_programa.java
@@ -0,0 +1,14 @@
+/**
+ * @param descripcion de la variable
+ * @return contenido que retorna
+ * @throws indica que metodo actual puede enviar una excepción
+ * @author
+ * @version
+ * @since cualquier descripcion de la version
+ * @deprecated indicar que no deberia utilizarse
+ */
+public class 1_ descripcion_programa{
+
+public static void main(String[]args){
+
+System.out.println("Hello World!")}}
