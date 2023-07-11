12e7aa3e14e6c2ab08d5ccb7379bb32b9d518ac3
yadevom <yaormu@gmail.com>
2023-07-10 23:04:58 -0500

    directorio makigas

---
+++ makigas/ovewriter/Animal.java
@@ -0,0 +1,13 @@
+/**
+ * Animal
+ */
+public class Animal {
+
+    public void caminar() {
+        System.out.println("El animal esta caminando");
+    }
+
+    public void sonido() {
+        System.out.println("El animal no emite ningun sonido");
+    }
+}
