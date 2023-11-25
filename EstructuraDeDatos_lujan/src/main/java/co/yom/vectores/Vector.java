699952027997d5613ec91a6a12a3e8209bb8e4b6
yadevom <yaormu@gmail.com>
2023-11-25 10:03:33 -0500

    vector

---
+++ EstructuraDeDatos_lujan/src/main/java/co/yom/vectores/Vector.java
@@ -0,0 +1,17 @@
+package co.yom.vectores;
+
+/**
+ * Java tiene una clase vector que contine una matriz que es cultivable de objetos
+ * osea se pueden ir colocando igual que una matriz
+ * Contine componentes que se puede acceder utilizando un indice entero
+ * Sin embargo el tamaña de un vector puede crecer o reducirse segun sea lo necesario
+ * para ir acomodando o eliminando los elementos despues de que se hallan creado
+ */
+public class Vector {
+
+    public static void main(String[] args) {
+
+        // Declarar un vector, esta vacío aparentemente porque posee inicialmente un tamaño de 10 elementos
+        Vector vector = new Vector();
+    }
+}
