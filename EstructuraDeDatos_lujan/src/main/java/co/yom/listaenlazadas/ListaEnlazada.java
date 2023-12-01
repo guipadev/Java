d0aa64fb4921855adbce4735ec0871826ff1be8d
yadevom <yaormu@gmail.com>
2023-12-01 06:40:34 -0500

    estrucutra lista enlazada

--- EstructuraDeDatos_lujan/src/main/java/co/yom/listaenlazadas/ListaEnlazada.java
+++ EstructuraDeDatos_lujan/src/main/java/co/yom/listaenlazadas/ListaEnlazada.java
@@ -1,7 +1,5 @@
 package co.yom.listaenlazadas;
 
-import java.util.List;
-
 public class ListaEnlazada {
 
     protected Nodo primero;
@@ -10,19 +8,24 @@ public class ListaEnlazada {
         primero = null;
     }
 
+    // Acciones de una lista normalmente
+
     public ListaEnlazada agregarInicio(int dato) {
         Nodo nuevo;
-        nuevo = new Nodo(dato);
-        nuevo.ref = primero;
+        nuevo = new Nodo(dato); // Al nodo nuevo le pasamos el dato
+        nuevo.ref = primero; // El nodo que siga sea el primero, el ultimo es el primero
         primero = nuevo;
         return this;
     }
 
-    public void agregarFinal() {}
+    public void agregarFinal() {
+    }
 
-    public void buscar() {}
+    public void buscar() {
+    }
 
-    public void eliminar() {}
+    public void eliminar() {
+    }
 
     public void verLista() {
         Nodo nodov;
